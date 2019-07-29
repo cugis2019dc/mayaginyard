7e5db76e5e6c7a8f0baf634242c623317ae71814
mayaginyard <53440452+mayaginyard@users.noreply.github.com>
2019-07-29 13:58:19 -0400

    Add files via upload

---
+++ spider.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+hello my name is Maya
+
+print("hello my name is Maya)
+"print(5/2)"
+ print(5-2)
+print(5**2)
+print(8/9*3)
+def multiply(a,b):
+    multiply= a*b
+    print(multiply)
+    
+    
+    
+multiply(4,5)
+
+def area(b,h):
+    area= (.5)*b*h
+    print("the area of the triangle with the' ,b base b and height h",area)
+    
+    
+area(3,4)
+     
+ 
